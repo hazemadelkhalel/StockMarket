5d6febd8706d96fc43dd9c31ff45c19cb17824e2
Hazem Adel <74595174+hazemadelkhalel@users.noreply.github.com>
2024-01-13 22:42:22 +0200

    initialize frontend of stock market

---
+++ Frontend/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Stock Market',
+  description: 'Stock Market application that allows multiple users to view/trade stocks',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
